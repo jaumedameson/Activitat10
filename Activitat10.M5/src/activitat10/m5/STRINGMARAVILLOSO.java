bb58e5352f8abdf6b1a1d8b4015fc443053ffcf3
jaume <jaume@DESKTOP-7VE5B1S>
2022-03-17 16:58:51 +0000

    Codi Esther Mètodes Finalitzats

--- Activitat10.M5/src/activitat10/m5/STRINGMARAVILLOSO.java
+++ Activitat10.M5/src/activitat10/m5/STRINGMARAVILLOSO.java
@@ -15,26 +15,11 @@ public class STRINGMARAVILLOSO{
  
         //Imprime los primeros dos triangulos de izq a derecha de la primera parte (parte superios)
         for (int i =0; i<M.length();i++){
-            partSuperiorEspaisT1(i);
-            partSuperiorTriangle1(i, M);
-            partSuperiorEspaisT2(i, M);
-            partSuperiorTriangle2(i, M);
+            partSuperior(i, M);
         }
         //Imprime los segundos dos triangulos de izq a derecha de la segunda parte (parte inferior)
         for (int i =0; i<M.length();i++){
-            //Imprime primer triangulo de la segunda parte
-            for (int j= i; j<M.length(); ++j){
-                System.out.print(M.charAt(j));
-            }
-            //Imprime espacios entre primer y segundo triangulo
-            for (int j=0; j <= i ;++j){
-                System.out.print(" ");
-            }
-            //Imprime segundo triangulo de la segunda parte  
-            for(int j=0; j < i + 1; ++j){
-                System.out.print(M.charAt(j));
-            }
-            System.out.println();
+            partInferior(i, M);
         }
     }    
 }
